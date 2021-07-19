e0656369d0b85795a60305fba86bcdbbb4b5c215
zaheenadeep <zaheen.a.deep@gmail.com>
2021-07-19 15:22:28 +0600

    Initial Commit

--- quickselect.py
+++ quickselect.py
@@ -1,8 +1,22 @@
+import random
+
+
 def _partition(arr, p, r):
-    pass
+    key = arr[r]
+    i = p - 1
+    for j in range(len(arr) - 1):
+        if arr[j] <= key:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[r] = arr[r], arr[i+1]
+    return i + 1
+
 
 def _randomized_partition(arr, p, r):
-    pass
+    i = random.randint(0, len(arr)-1)
+    arr[i], arr[r] = arr[r], arr[i]
+    return _partition(arr, p, r)
+
 
 def _quickselect_helper(arr, p, r, i):
     if p == r:
@@ -14,7 +28,16 @@ def _quickselect_helper(arr, p, r, i):
     elif k < i - 1:
         return _quickselect_helper(arr, p, q-1, i)
     else:
-         return _quickselect_helper(arr, q+1, r,  )
+        return _quickselect_helper(arr, q+1, r, i-k-1)
+
 
 def quickselect(arr, i):
+    if i < 1 or i > len(arr):
+        raise LookupError
     return _quickselect_helper(arr, 0, len(arr) - 1, i)
+
+
+if __name__ == '__main__':
+    arr = [7, 4, 2, 5, 6, 2, 1, 5, 54]
+    print(quickselect(arr, 6))  # 2
+    print(quickselect(arr, 45))  # 2
