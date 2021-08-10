5b539d1ff7cae115204dc8be5d8ab236510c8ca7
zaheenadeep <zaheen.a.deep@gmail.com>
2021-08-10 22:29:58 +0600

    On Progress

--- bst.py
+++ bst.py
@@ -1,6 +1,7 @@
 class TreeNode:
-    def __init__(self, key, left = None, right = None):
+    def __init__(self, key, parent = None, left = None, right = None):
         self.key = None
+        self.parent = parent
         self.left = left
         self.right = right
 
@@ -16,10 +17,7 @@ class TreeNode:
         pass
 
     def _max(self):
-        pass
-
-    def _search(self, key):
-        pass
+        while
 
     def _recursive_search(self, key):
         pass
@@ -32,7 +30,7 @@ class BinarySearchTree:
 
     def inorder_walk(self):
         """ Prints every element in the tree in sorted order"""
-        self._inorder_walk(self.root)
+        BinarySearchTree._inorder_walk(self.root)
 
     @staticmethod
     def _inorder_walk(localroot):
@@ -40,3 +38,26 @@ class BinarySearchTree:
             BinarySearchTree._inorder_walk(localroot.left)
             print(localroot.key)
             BinarySearchTree._inorder_walk(localroot.right)
+
+    def search(self, key):
+        """ Return the node which contains the specified key """
+        localroot = self.root
+        while localroot != None or localroot != key:
+            if key < localroot:
+                localroot = localroot.left
+            else:
+                localroot = localroot.right
+        return localroot
+
+
+    def recursive_search(self, key):
+        return BinarySearchTree._recursive_search(self.root, key)
+
+    @staticmethod
+    def _recursive_search(localroot, key):
+        if localroot == None or localroot.key == key:
+            return localroot
+        elif key < localroot.key:
+            return BinarySearchTree.search(localroot.left, key)
+        else:
+            return BinarySearchTree.search(localroot.right, key)
